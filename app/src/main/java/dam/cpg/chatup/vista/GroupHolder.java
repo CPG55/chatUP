6426942a0f59ef3bc6fd303ad842ee5275154356
Carlos <carlos@optimov.com>
2018-06-15 11:14:23 +0200

    Add Main Activity RecyclerView / Fix partial bug.
    Add recyclerview row view: group_item.
    Add Group entity.
    Add holder and adapter for recyclerview.
    Add icon for default profile user's picture.
    The recyclerView has no data yet.
    Fix a bug that were making the app to make a loop in "onResume()" method between MainActivity and LoginActivity when trying to login, not creating  a  new account. The  new lines introduced are checking if there is  an instance of Authentication that is not null before launch login screen again. This  "fix" introduced a new bug, now when you register a new account the app crashes after signup, but account is succesful created, and can be used for login.

---
+++ app/src/main/java/dam/cpg/chatup/vista/GroupHolder.java
@@ -0,0 +1,35 @@
+package dam.cpg.chatup.vista;
+
+import android.support.v7.widget.RecyclerView;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+import dam.cpg.chatup.R;
+import dam.cpg.chatup.modelo.Group;
+import de.hdodenhof.circleimageview.CircleImageView;
+
+/**
+ * @author Carlos Pérez on 13/06/18.
+ */
+class GroupHolder extends RecyclerView.ViewHolder {
+
+    @BindView(R.id.user_name) TextView _userName;
+    @BindView(R.id.user_profile_picture) CircleImageView _userProfilePicture;
+    //@BindView(R.id.user_last_message) TextView _userLastMessage;
+
+    GroupHolder(View itemView) {
+        super(itemView);
+
+        ButterKnife.bind(itemView);
+
+    }
+
+    void bind(Group group) {//
+        _userName.setText(group.getOppositeUser().getName());
+        //_userProfilePicture.setImageURI();
+    }
+
+}
